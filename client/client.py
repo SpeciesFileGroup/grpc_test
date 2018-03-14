c3e4870e1aa65cc8acfe0f90ca3c022e9f3757c5
Moogen <jwu9995@gmail.com>
2018-03-14 14:00:09 -0500

    Fixed client

--- client/client.py
+++ client/client.py
@@ -2,13 +2,13 @@
 # The client asks each server a question, receives an answer from each server, and then concatenates their answers. 
 
 import grpc 
-import question_pb2.py as q
-import question_pb2_grpc.py as qgrpc
+import question_pb2 as q
+import question_pb2_grpc as qgrpc
 
 def run(): 
 	channel = grpc.insecure_channel('localhost:50051')
-	stub = qgrpc.QuestionServiceStub(cannel)
-	response = stub.UnaryRequest(q.UnaryRequest(query="Hello?"))
+	stub = qgrpc.QuestionServiceStub(channel)
+	response = stub.UnaryRequest(q.QuestionRequest(query="Hello?"))
 	print("Server response received: " + response.message)
 
 if __name__ == '__main__':
