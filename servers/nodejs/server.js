02f3a7b3d86460225e5182e3221d03eaf6bde2fb
Ankit Gohel <ankitgohel1996@gmail.com>
2018-03-19 16:41:10 -0500

    Simple RPC working

--- servers/nodejs/server.js
+++ servers/nodejs/server.js
@@ -1,31 +1,18 @@
 var PROTO_PATH = '../../protos/question.proto';
 var grpc = require('grpc');
 var protoDescriptor = grpc.load(PROTO_PATH);
-console.log(protoDescriptor)
 var routeguide = protoDescriptor.QuestionService;
 
 function checkResponse(query) {
 	var QuestionResponse;
 	QuestionResponse = {
-		name : '',
 		reply : '1'
 	};
 	return QuestionResponse;
 }
 
-// function UnaryRequest(call, callback) {
-// 	callback(null, checkResponse(call.request))
-// }
-
-function UnaryRequest(call) {
-	console.log(call)
-	var QuestionResponse;
-	QuestionResponse = {
-		name : '',
-		reply : '1'
-	};
-	console.log(QuestionResponse)
-	return QuestionResponse;
+function UnaryRequest(call, callback) {
+	callback(null, checkResponse(call.request))
 }
 
 function getServer() {
