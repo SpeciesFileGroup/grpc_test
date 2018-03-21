455c376edb93af646a5c6a51b3295d3cc94ee8f7
Ankit Gohel <ankitgohel1996@gmail.com>
2018-03-21 00:21:53 -0500

    Added client function to receive stream of data

--- client/client.py
+++ client/client.py
@@ -4,6 +4,8 @@
 import grpc 
 import question_pb2 as q
 import question_pb2_grpc as qgrpc
+import questionstream_pb2 as q1
+import questionstream_pb2_grpc as qgrpc1
 
 def run(): 
 	channel = grpc.insecure_channel('localhost:50051')
@@ -14,5 +16,16 @@ def run():
 	else:
 		print("Server response received: " + str(response))
 
+def run_stream(): 
+	channel = grpc.insecure_channel('localhost:50051')
+	stub = qgrpc1.QuestionServiceStub(channel)
+	
+	question = q1.QuestionRequest(query="Hello?")
+	answers = stub.UnaryRequest(question)
+	print(answers)
+
+	for answer in answers:
+		print("Server response received: " + str(answer))
+
 if __name__ == '__main__':
-	run()
+	run_stream()
