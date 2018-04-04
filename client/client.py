98e19ea28a6d08074b28dd15ab574c49e32971b5
Ankit Gohel <ankitgohel1996@gmail.com>
2018-04-04 12:55:35 -0500

    Streaming client works

--- client/client.py
+++ client/client.py
@@ -3,10 +3,16 @@
 
 import grpc 
 import sys, time
+import os, pprint
+from collections import defaultdict
+from os import listdir
+from os.path import isfile, join
 import question_pb2 as q
 import question_pb2_grpc as qgrpc
 import questionstream_pb2 as q1
 import questionstream_pb2_grpc as qgrpc1
+
+pp = pprint.PrettyPrinter(indent=4)
 
 def run(): 
 	channel = grpc.insecure_channel('localhost:50051')
@@ -17,26 +23,38 @@ def run():
 	else:
 		print("Server response received: " + str(response))
 
-def generate_stream():
-	# files = [f for f in listdir(directory) if isfile(join(directory, f))]
-
-	files = ['a','b','c']
+def generate_stream(files):
 	for file in files:
-		yield q1.QuestionRequest(query = str(file))
-		time.sleep(0.2)
+		with open(file, 'r') as my_file:
+			file_data = my_file.read().replace('\n','')
+			yield q1.QuestionRequest(query = file_data)
 
-def run_stream(directory): 
+#Creates a stream of requests, should be able to use this with any server
+# Due to some weird issues with node, I'm not able to generate the protobuf code, so as a workaround, the answer you get back and the message you send have the same field nanmed 'query'. Hacky, but i'm looking at how to fix it
+def run_stream(files): 
 	channel = grpc.insecure_channel('localhost:50051')
 	stub = qgrpc1.QuestionServiceStub(channel)
-	
-	answers = stub.UnaryRequest(generate_stream())
 
+	answers = stub.UnaryRequest(generate_stream(files))
+	word_count = defaultdict(int)
+
+	i = 0
 	try:
 		for answer in answers:
-			print(str(answer))
+			word_count[files[i]] = answer.query
+			i += 1	
 	except grpc._channel._Rendezvous as err:
 		print(err)
 
+	pp.pprint(word_count)
+
 if __name__ == '__main__':
 	directory = sys.argv[1]
-	run_stream(directory)
+	directory = os.path.abspath(directory)
+
+	files = []
+	for root, directories, filenames in os.walk(directory):
+		for filename in filenames:
+			files.append(os.path.join(root,filename))
+
+	run_stream(files)
